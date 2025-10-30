79363414eeb4b934a29b5b767cdec20a8887815a
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-10-30 12:26:43 -0300

    feat: add UserService for user creation functionality

---
+++ backend/app/Services/UserService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserService
+{
+    public function createUser(array $data): User
+    {
+        $user = User::create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $user;
+    }
+}
