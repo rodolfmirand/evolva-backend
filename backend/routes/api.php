f74e549bfab9dcb2819fe7e0a84d5d185389b675
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-10-31 08:16:36 -0300

    feat: group authenticated routes for better organization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::patch('/users/{user}', [AuthController::class, 'update']);
 });
-
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
