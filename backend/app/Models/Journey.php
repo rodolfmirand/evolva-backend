72cf9fd5496b854e96bfac7f86ff4fe321b8ae28
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-01 17:08:17 -0300

    style: format code for consistency and readability in Journey model

--- backend/app/Models/Journey.php
+++ backend/app/Models/Journey.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Journey extends Model {
+class Journey extends Model
+{
     /**
      * Criar dados falsos para testes, pode remover depois
      * exemplo: Journey::factory()->count(10)->create();
@@ -17,21 +18,28 @@ class Journey extends Model {
         'title',
         'description',
         'is_private',
-        ];
+    ];
 
-    protected static function booted() {
+    protected $casts = [
+        'is_private' => 'boolean',
+    ];
+
+    protected static function booted()
+    {
         static::creating(function ($journey) {
             $journey->join_code = strtoupper(Str::random(6)); //TODOIN: melhorar essa geração de código
         });
     }
 
-    public function users() {
+    public function users()
+    {
         return $this->belongsToMany(User::class, 'journey_user')
-                    ->withPivot('is_master')
-                    ->withTimestamps();
+            ->withPivot('is_master')
+            ->withTimestamps();
     }
 
-    public function tasks() {
+    public function tasks()
+    {
         return $this->hasMany(Task::class);
     }
 }
