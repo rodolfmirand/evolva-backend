07f698c1e04e68a4905521f9f9385031d62165b0
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-04 10:57:01 -0300

    feat: update routes to use UserController for user registration and updates and add route to return user's journeys

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,14 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
-        return $request->user();
+        return $request->user(); // retorna os dados do usuário autenticado
     });
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::patch('/users/{user}', [AuthController::class, 'update']);
+    Route::patch('/users/{user}', [UserController::class, 'update']);
+    Route::get('/users/{userId}/journeys', [UserController::class, 'journeys']);
+
 });
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
