3d9430aa73c4f4a7a43f3bf4c30f23cf9c0057c2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-04 10:54:42 -0300

    chore: change de local of auth controller and remove user's CRUD methods

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -16,39 +16,6 @@ class AuthController extends Controller
 {
     public function __construct(protected UserService $userService) {}
 
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Password::min(8)],
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'errors' => $validator->errors()
-            ], 422); // 422 Unprocessable Entity
-        }
-
-        try {
-            $user = $this->userService->createUser($validator->validated());
-
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-            return response()->json([
-                'message' => 'Usuário registrado com sucesso!',
-                'user'    => $user,
-                'token'   => $token,
-            ], 201); // 201 Created
-
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Erro ao registrar usuário',
-                'error'   => $e->getMessage()
-            ], 500); // 500 Internal Server Error
-        }
-    }
-
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -88,14 +55,5 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Logout realizado com sucesso!'
         ]);
-    }
-
-    public function update(UpdateUserRequest $request, User $user)
-    {
-        $validatedData = $request->validated();
-
-        $updatedUser = $this->userService->updateUser($user, $validatedData);
-
-        return new UserResource($updatedUser);
     }
 }
