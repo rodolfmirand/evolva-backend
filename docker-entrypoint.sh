2d5c6db5ddc7c91d2ff5d17302c780af424113cd
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-07 17:40:37 -0300

    refactor: remove database wait and migration commands from entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Aguardando banco de dados MySQL ficar disponível..."
-
-sleep 15
-
-php artisan migrate --force
-
 # Inicia o PHP-FPM
 echo "Iniciando PHP-FPM..."
 exec php-fpm
